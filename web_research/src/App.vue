6cf4a272869765543b531c7fbf32c2fef45e60f2
ID - Jose Luis Gomez <jlgomez@fermax.com>
2024-12-23 11:16:32 +0100

    Install and configure vuetify and vuetify plugin for vite

--- web_research/src/App.vue
+++ web_research/src/App.vue
@@ -3,22 +3,28 @@ import { ERoutes} from './router/routes'
 </script>
 
 <template>
-  <nav>
-      <ul>
-        <li>
-          <!-- Usar router-link con la ruta definida en el enum -->
-          <router-link :to="{ path: ERoutes.LOGIN_VIEW }">Login</router-link>
-        </li>
-        <li>
-          <router-link :to="{ path: ERoutes.MAIN_VIEW }">Main</router-link>
-        </li>
-        <li>
-          <router-link :to="{ path: ERoutes.UNKNOWN_VIEW }">Unknown</router-link>
-        </li>
-      </ul>
-    </nav>
 
-  <RouterView/>
+  <v-app>
+   
+    <v-card class="elevation-12">
+      <v-btn color="primary">Botón</v-btn>
+    </v-card>
+    <nav>
+        <ul>
+          <li>
+            <!-- Usar router-link con la ruta definida en el enum -->
+            <router-link :to="{ path: ERoutes.LOGIN_VIEW }">Login</router-link>
+          </li>
+          <li>
+            <router-link :to="{ path: ERoutes.MAIN_VIEW }">Main</router-link>
+          </li>
+          <li>
+            <router-link :to="{ path: ERoutes.UNKNOWN_VIEW }">Unknown</router-link>
+          </li>
+        </ul>
+      </nav>
+    <RouterView/>
+  </v-app>
 </template>
 
 <style scoped>
--- web_research/src/main.ts
+++ web_research/src/main.ts
@@ -1,13 +1,36 @@
-import './style.css'
+//import '@mdi/font/css/materialdesignicons.css';
+
+import 'vuetify/styles';
+
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 import App from './App.vue'
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import { createVuetify } from 'vuetify';
 import router from './router/index.ts';
 
-const app = createApp(App);
-//app.use(vuetify);
-app.use(router);
-//app.use(AxiosPlugin, { baseUrl: baseUrl })
-//app.use(pinia)
-app.mount('#app');
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light',       // Establece el tema predeterminado
+    themes: {
+      light: {
+        colors: {
+          primary: '#005196',    // Azul personalizado
+          secondary: '#FFCDD2',  // Color secundario claro (anteriormente colors.red.lighten4)
+          accent: '#3F51B5',     // Color de acento (anteriormente colors.indigo.base)
+        },
+      },
+    },
+  },
+  icons: {
+    defaultSet: 'mdi',  // Usa MDI como el conjunto de íconos por defecto
+  },
+});
 
+const app = createApp(App);
+app.use(vuetify);
+app.use(router);
+app.mount('#app');
